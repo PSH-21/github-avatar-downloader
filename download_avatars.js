7c6306362b9ebdce14bbccf9bf9c8f7b2b67e0b3
Peter Hunt <peter.s.hunt@gmail.com>
2017-09-12 00:02:05 +0000

    create the requesturl

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,13 @@
-var request = require('request');
+require('dotenv').config()
+const request = require('request');
 
+var GITHUB_USER = "PSH-21";
+var repoOwner = 'jquery';
+var repoName = 'jquery';
 
+var requestURL = 'https://'+ GITHUB_USER + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+console.log(requestURL);
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
